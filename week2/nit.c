eebf3ed9cff3e52dbac7ecb3ffbe47b9ac54f681
Your Name <you@example.com>
2017-03-13 09:58:32 +0530

    aa

---
+++ week2/nit.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int i,n,sum=0;
+printf("enter the number of terms to add\n %d",n);
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+{
+sum=sum+i;
+}
+scanf("sum = %d",&sum);
+}
+
